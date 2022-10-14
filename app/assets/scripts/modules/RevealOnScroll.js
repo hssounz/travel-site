a90a13a964655139f7792b9546726e0979650241
Hassen <hassenbensaad55@gmail.com>
2022-10-14 14:40:08 +0200

    finishin reveal on scroll

---
+++ app/assets/scripts/modules/RevealOnScroll.js
@@ -0,0 +1,30 @@
+import $ from 'jquery';
+import waypoints from '../../../../node_modules/waypoints/lib/noframework.waypoints';
+
+
+export default class RevealOnScroll {
+
+    constructor() {
+        this.features = document.querySelectorAll('#feature, .card');
+        this.handleFeatures();
+
+    }
+
+
+
+    handleFeatures = () => {
+        this.features.forEach(item => {
+            item.classList.add("reveal-item");
+
+            new Waypoint({
+                element: item,
+                handler: () => {
+                    item.classList.add("reveal-item--visibilee");
+                },
+                offset: '85%' ,
+            });
+        });
+    }
+
+
+}
