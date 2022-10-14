5b70104f42871773a2bd13fe3273a5b72d10dbed
Hassen <hassenbensaad55@gmail.com>
2022-10-14 20:16:33 +0200

    Finishin sticky head with smooth scroll and active links

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,6 +1,8 @@
 import MobileMenu from "./modules/MobileMenu";
 import RevealOnScroll from "./modules/RevealOnScroll";
+import NavSticky from "./modules/NavSticky";
 
 let menu = new MobileMenu();
 let revealOnScroll = new RevealOnScroll();
+let navSticky = new NavSticky();
 
